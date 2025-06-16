c5d792c0557762447b506e74f530f6047ecd5381
Yersin <yersinme@gmail.com>
2025-06-16 17:28:03 +0500

    commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,14 +4,14 @@ import MainSubImg from '../../assets/img/main-sub-img.svg';
 
 const Main = () => {
     return (
-        <div className="main-item">
-            <div className='main-item-content container'>
-                <div className='main-item-content-item'>
-                    <h1 className='main-item-content-item-text'>Silk Era Road <br/> 23-24 сентября 2025</h1>
+        <div className="main">
+            <div className='main-content container'>
+                <div className='main-content-item'>
+                    <h1 className='main-content-item-text'>Silk Era Road <br/> 23-24 сентября 2025</h1>
                     <Button title="Зарегистрироваться "/>
                 </div>
 
-                <div className='main-item-content-text-sub'>
+                <div className='main-content-text-sub'>
                     <img src={MainSubImg} alt="img"
                          className='main-item-content-text-sub-img'/>
                 </div>
